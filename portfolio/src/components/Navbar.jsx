23b2c125f9320834f9aab623f2cf2c83ef30985e
arybyt <bayatiarya@gmail.com>
2021-08-07 14:52:41 -0500

    picture position in navbar started

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -5,25 +5,20 @@ import './Navbar.css'
 
 const Nav = ({ user }) => {
   return (
-    <div className='siya'>
-      <img className="meImg" src="siya.jpg" />
-        
+    <div className='meImg'>
+      <img className="bg" src="https://thumbs-prod.si-cdn.com/zwOzXtZnnwCaVDxKFIg6N-QNAiU=/fit-in/1600x0/https://public-media.si-cdn.com/filer/25/f0/25f08cff-0355-4001-86ad-28950a866af4/grandteton.jpg" alt=""  />
+      <img className="img" src="siya.jpg" alt="" />
+      <div>
     <nav>
-        
-      <img className="backimg" src="img2.jpg" />
-    
       <div className="nav-bar" >
         <Link className="link" to="/" >About</Link>
         <Link className="link" to="/projects">Projects</Link>
         <Link className="link" to="/resume">Resume</Link>
         <div className="links">
-
-
-
-
         </div>
       </div>
-      </nav>
+        </nav>
+        </div>
       </div>
   )
 }
