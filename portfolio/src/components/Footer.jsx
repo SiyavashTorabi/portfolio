9705403e015043a7c74b863dfb197829986a25ec
siyavashtorabi <vashsiya81@gmail.com>
2021-08-10 13:12:02 -0500

    need to add the resume screen

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-
+import "./Footer.css"
 export default function Footer() {
   return (
-    <footer className="footer mt-5 ">
-      <img id="github" src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/github_fpykxh.png" alt="github" className="icons" />
-      <h5>@siyavashtorabi</h5>
+    <footer className="footer">
+      <img className="icon" id="github" src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/github_fpykxh.png" alt="github" className="icons" />
+      <a className="githubL" href="https://github.com/SiyavashTorabi/" target="_blank">@Siyavash Torabi</a>
+      <img className="instaIcon" id="instagram" src="https://image.similarpng.com/very-thumbnail/2020/05/Instagram-logo-with-brush-PNG.png" alt="intagram" className="icons" />
+      <a className="githubL" href="https://www.instagram.com/siya_mashti/" target="_blank">@Siya_mashti</a>
+      <img className="instaIcon" id="instagram" src="https://logodix.com/logo/79657.png" alt="intagram" className="icons" />
+      <a className="githubL" href="https://www.linkedin.com/in/siyavash-torabi/" target="_blank">@Siyavash torabi</a>
+    
+
+      <h3 className='contact' >Email: Vashsiya81@gmail.com</h3>
+      <h4 className='contact' >Phone Number: 737-240-5023</h4>
     </footer>
   )
 }
