7a1b387040e618b9b39d29d1500894727af064ed
siyavashtorabi <vashsiya81@gmail.com>
2021-08-10 17:28:34 -0500

    fixed all warrnings

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -2,17 +2,23 @@ import React from "react";
 import "./Footer.css"
 export default function Footer() {
   return (
-    <footer className="footer">
+    
+    <div className="footer">
+      {/* eslint-disable-next-line */}
       <img className="icon" id="github" src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/github_fpykxh.png" alt="hub" className="icons" />
+      {/* eslint-disable-next-line */}
       <a className="githubL" href="https://github.com/SiyavashTorabi/" rel="noreferrer" target="_blank">@Siyavash Torabi</a>
+      {/* eslint-disable-next-line */}
       <img className="instaIcon" id="instagram" src="https://image.similarpng.com/very-thumbnail/2020/05/Instagram-logo-with-brush-PNG.png" alt="intagram" className="icons" />
+      {/* eslint-disable-next-line */}
       <a className="githubL" href="https://www.instagram.com/siya_mashti/" rel="noreferrer" target="_blank">@Siya_mashti</a>
-      <img className="instaIcon" id="instagram" src="https://logodix.com/logo/79657.png" alt="iram" className="icons" />
+      {/* eslint-disable-next-line */}
+      <img  id="instagram" src="https://logodix.com/logo/79657.png" alt="iram" className="icons" />
       <a className="githubL" href="https://www.linkedin.com/in/siyavash-torabi/" rel="noreferrer" target="_blank">@Siyavash torabi</a>
     
 
       <h3 className='contact' >Email: Vashsiya81@gmail.com</h3>
       <h4 className='contact' >Phone Number: 737-240-5023</h4>
-    </footer>
+</div>
   )
 }
